4fad4fc4ee68870848063be74abc637d19f56541
Ashish Sunkarapalli <ashishs5709@gmail.com>
2026-05-18 14:33:26 -0400

    Implemented the home screen

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        Hi, I'm Ashish. I'm an undergraduate student and developer passionate about robotics, data-driven policy, and building impactful software.
+      </p>
+      <p>Feel free to explore my projects or get in touch!</p>
+    </div>
+  );
+}
+
+export default Home;
